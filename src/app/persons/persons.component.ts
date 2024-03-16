adeb2e52b247da7f2c6f5c5f296ac16ace6f3949
Tomer Kedem <tompha1@hotmail.com>
2024-03-16 20:11:43 +0200

    name fixed

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -24,19 +24,19 @@ export class PersonsComponent implements OnInit{
       id: 1,
       name: 'Hardik Savani',
       gender: 0,
-      website: 'itsolutionstuff.com'
+      website: 'itsolution.com'
     },
     {
       id: 2,
       name: 'Kajal Patel',
       gender: 1,
-      website: 'nicesnippets.com'
+      website: 'nicetohave.com'
     },
     {
       id: 3,
       name: 'Harsukh Makawana',
       gender: 0,
-      website: 'laracode.com'
+      website: 'makawana.com'
     }
   ]
 }
